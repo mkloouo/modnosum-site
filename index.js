4707baae05383fcc5813dfa3059f56af56469474
Mykola Odnosumov <modnosum@gmail.com>
2018-10-01 12:35:22 +0300

    Add Daniel's Shiffman coding challenge

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let counter = 0;
 
 function buttonClicked() {
 	let btn = document.getElementById('click-me-btn');
-	btn.innerText = 'You clicked me ' + (++counter).toString() + 'times!';
+	btn.innerText = 'You clicked me ' + (++counter).toString() + ' times!';
 }
